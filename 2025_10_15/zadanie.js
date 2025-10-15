075775364021e64b22123604a22866e55c8fb4d4
JanTopolewski <jankotopolewski@gmail.com>
2025-10-15 10:36:18 +0200

    Add a solution to task from 15.10

---
+++ 2025_10_15/zadanie.js
@@ -0,0 +1,71 @@
+let http = require('http')
+const url = require('url')
+const fs = require('fs').promises
+const mime = require('mime-types')
+const path = require('path')
+
+http.createServer(async (req, res) => {
+    let urlObject = url.parse(req.url, true)
+    let timestamp = Date.now()
+    switch(urlObject.pathname){
+        case '/homepage':
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'})
+            res.write('Strona główna')
+            res.end()
+            break
+        case '/jsondocument':
+            res.writeHead(200, {'Content-Type': 'text/json'})
+            let jsonData = {name: "Janek", class: '4D'}
+            res.write(JSON.stringify(jsonData))
+            res.end()
+            break
+        case '/htmlcode':
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            let code = `<html>
+                <head>
+                    <meta charset="utf-8"/>
+                    <title>Kod HTML</title>
+                </head>
+                <body>
+                    <h1>Przedmioty szkolne</h1>
+                    <ol>
+                        <li>j. polski</li>
+                        <li>matematyka</li>
+                        <li>j.angielski</li>
+                        <li>informatyka</li>
+                    </ol>
+                </body>
+            </html>`
+            res.write(code)
+            res.end()
+            break
+        case '/htmlfile':
+            const file = await fs.readFile('strona.html', 'utf8')
+            res.end(file)
+            break
+        case '/get_params':
+            console.log(JSON.stringify(urlObject.query, null, 2))
+            res.writeHead(200, {'Content-Type': 'text/json'})
+            await fs.writeFile(`params_${timestamp}.json`, JSON.stringify(urlObject.query, null, 2))
+            let jsonInformation = {ok: 'ok'}
+            res.write(JSON.stringify(jsonInformation))
+            res.end()
+            break
+        default:
+            try{
+                await fs.stat(path.join(__dirname, 'assets', urlObject.pathname))
+                fileType = mime.lookup(urlObject.pathname)
+                res.setHeader('Content-Type', fileType)
+                res.statusCode = 200
+                let fileContent = await fs.readFile(path.join(__dirname, 'assets', urlObject.pathname), 'utf8')
+                res.end(fileContent)
+            }
+            catch(error){
+                res.statusCode = 404
+                res.setHeader('Content-Type', 'application/json')
+                let answer = {error: "Not found!"}
+                res.end(JSON.stringify(answer))
+            }
+    }
+
+}).listen(8080, '127.0.0.1')
