1200bd3e0b47376ea6ec3d07aca9aec8449d910c
JanTopolewski <jankotopolewski@gmail.com>
2025-10-29 10:44:46 +0100

    Add a solution to task from 29.10

---
+++ 2025_10_29/zadanie.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/homepage.html'))
+})
+
+app.get('/o-nas', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/about.html'))
+})
+
+app.get('/oferta', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/offer.html'))
+})
+
+app.get('/kontakt', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/contact.html'))
+})
+
+app.post('/kontakt', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send({error: "Not found!"})
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
