1db4cc68d473af3053a01159de8966c56d0ab839
JanTopolewski <jankotopolewski@gmail.com>
2025-11-26 03:34:42 +0100

    Enhance API with error handling and CRUD operations
    Refactor API endpoints to include error handling and support for PATCH and DELETE methods.

--- 2025_11_19/index.js
+++ 2025_11_19/index.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient, Prisma } = require('@prisma/client')
 const express = require('express')
 const dotenv = require('dotenv')
 
@@ -9,64 +9,251 @@ const prisma = new PrismaClient()
 dotenv.config()
 app.use(express.json())
 
-app.get("/post", async (req, res) => {
-    const posts = await prisma.post.findMany({})
-    res.json(posts)
+app.get("/post", async (req, res,next) => {
+    try {
+        const posts = await prisma.post.findMany({})
+        res.json(posts)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
-app.get("/post/:id", async (req, res) => {
-    const id = req.params.id
-    const post = prisma.post.findUnique({
-        where: {
-            id: id
+app.get("/post/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const post = await prisma.post.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        if(post) {
+            res.json(post)
         }
-    })
-    res.json(post)
-})
-
-app.post("/post", async (req, res) => {
-    await prisma.post.create({data: req.body});
-    res.status(201).json({"ok": "Ok"})
-})
-
-app.get("/comment", async (req, res) => {
-    const posts = await prisma.comment.findMany({})
-    res.json(posts)
-})
-
-app.get("/comment/:id", async (req, res) => {
-    const id = req.params.id
-    const post = prisma.comment.findUnique({
-        where: {
-            id: id
+        else{
+            res.status(404).json({message: "Not Found"})
         }
-    })
-    res.json(post)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
-app.post("/comment", async (req, res) => {
-    await prisma.comment.create({data: req.body});
-    res.status(201).json({"ok": "Ok"})
+app.post("/post", async (req, res, next) => {
+    try {
+        const created = await prisma.post.create({data: req.body})
+        res.status(201).json(created)
+    }
+    catch(err){
+        next(err)
+    }
 })
 
-app.get("/category", async (req, res) => {
-    const posts = await prisma.category.findMany({})
-    res.json(posts)
+app.patch("/post/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const updatedPost = req.body
+
+        const updated = await prisma.post.update({
+            where: {
+                id: id
+            },
+            data: updatedPost
+        })
+
+        res.json(updated)
+    }
+    catch(err){
+        next(err)
+    }
 })
 
-app.get("/category/:id", async (req, res) => {
-    const id = req.params.id
-    const post = prisma.category.findUnique({
-        where: {
-            id: id
+app.delete("/post/:id(\\d+)", async (req, res, next) => {
+    try {
+        await prisma.post.delete({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        })
+
+        res.json({message: "ok"})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+
+app.get("/comment", async (req, res,next) => {
+    try {
+        const comments = await prisma.comment.findMany({})
+        res.json(comments)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+app.get("/comment/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const comment = await prisma.comment.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        if(comment) {
+            res.json(comment)
         }
-    })
-    res.json(post)
+        else{
+            res.status(404).json({message: "Not Found"})
+        }
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
-app.post("/category", async (req, res) => {
-    await prisma.category.create({data: req.body})
-    res.status(201).json({"ok":"Ok"})
+app.post("/comment", async (req, res, next) => {
+    try {
+        const created = await prisma.comment.create({data: req.body})
+        res.status(201).json(created)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+app.patch("/comment/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const updatedComment = req.body
+
+        const updated = await prisma.comment.update({
+            where: {
+                id: id
+            },
+            data: updatedComment
+        })
+
+        res.json(updated)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+app.delete("/comment/:id(\\d+)", async (req, res, next) => {
+    try {
+        await prisma.comment.delete({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        })
+
+        res.json({message: "ok"})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+
+app.get("/category", async (req, res,next) => {
+    try {
+        const categories = await prisma.category.findMany({})
+        res.json(categories)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+app.get("/category/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const category = await prisma.category.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        if(category) {
+            res.json(category)
+        }
+        else{
+            res.status(404).json({message: "Not Found"})
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+app.post("/category", async (req, res, next) => {
+    try {
+        const created = await prisma.category.create({data: req.body})
+        res.status(201).json(created)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+app.patch("/category/:id(\\d+)", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const updatedCategory = req.body
+
+        const updated = await prisma.category.update({
+            where: {
+                id: id
+            },
+            data: updatedCategory
+        })
+
+        res.json(updated)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+app.delete("/category/:id(\\d+)", async (req, res, next) => {
+    try {
+        await prisma.category.delete({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        })
+
+        res.json({message: "ok"})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+app.all("*", (req, res) => {
+    res.status(404).send("Not Found")
+})
+
+app.use((err, req, res, next) => {
+    console.error(err)
+
+    if(err instanceof Prisma.PrismaClientKnownRequestError) {
+        if(err.code === "P2025"){
+            return res.status(404).json({message: "Record not Found"})
+        }
+    }
+
+    if (err instanceof Prisma.PrismaClientValidationError) {
+        return res.status(400).json({message: "Validation failed" })
+    }
+
+    res.status(500).json({message: "Internal Server Error"})
 })
 
 app.listen(port, () => {
