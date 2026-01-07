3c43c748027dc7c051e50b4a793905eddd8bfc72
JanTopolewski <jankotopolewski@gmail.com>
2026-01-07 12:11:58 +0100

    Add first part of solution to task from 7.01

---
+++ 2026_01_07/src/pages/BlogPost.tsx
@@ -0,0 +1,13 @@
+export default function BlogPost() {
+    return (
+        <div>
+            <h1>Polecany przepis na bigos</h1>
+            <h5>Kategoria: Gastronomia</h5>
+            <ol>
+                <li>Składniki
+
+                </li>
+            </ol>
+        </div>
+    )
+}
---
+++ 2026_01_07/src/pages/Categories.tsx
@@ -0,0 +1,14 @@
+export default function Categories() {
+    return (
+        <div>
+            <h1>Kategorie wpisów</h1>
+            <ul>
+                <li>Gastronomia</li>
+                <li>Turystyka</li>
+                <li>Technologia</li>
+                <li>Sport</li>
+                <li>Edukacja</li>
+            </ul>
+        </div>
+    )
+}
