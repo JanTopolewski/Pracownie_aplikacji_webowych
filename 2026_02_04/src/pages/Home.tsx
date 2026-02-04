d216dc12fd18133fa8beea6b278ac1b4cbfb463a
JanTopolewski <jankotopolewski@gmail.com>
2026-02-04 09:34:39 +0100

    Add first part of the solution to task from 4.02

---
+++ 2026_02_04/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div>
+            <h1>Witaj na moim blogu!</h1><hr/>
+            <p>Zapraszam do zapoznania się ze świetnymi codziennymi informacjami, opiniami i pomysłami związanymi z życiem codziennym oraz światem technologii i sportu. </p>
+        </div>
+    )
+}
