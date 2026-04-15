e62ea262a54f96d91293901c91ed56eb1406be13
JanTopolewski <jankotopolewski@gmail.com>
2026-04-15 07:59:57 +0200

    Add the second part of the solution to the task from 4.08

--- 2026_04_08/task_solution.py
+++ 2026_04_08/task_solution.py
@@ -1,34 +1,63 @@
-class ListElement:
-    def __init__(self):
-        self.next = None
-        self.v = 0
-
 def read_graph(filename: str):
     with open(filename, "r") as file:
-        v_number = int(file.readline().strip())
-        v_tab = [None] * v_number
-        for vertex_data in file:
-            vertex_data_list = vertex_data.split()
-            previous_vertex = None
-            for i in range(len(vertex_data_list) - 1, 0, -1):
-                p = ListElement()
-                p.v = int(vertex_data_list[i])
+        vertices_number = int(file.readline().strip())
+        adjacency_list = [[]] * vertices_number
 
-                if previous_vertex:
-                    p.next = previous_vertex
+        for vertices_data in file:
+            vertices_data_list = vertices_data.split()
+            for i in range(1, len(vertices_data_list)):
+                adjacency_list[int(vertices_data_list[0])].append(int(vertices_data_list[i]))
 
-                previous_vertex = p
-
-            v_tab[int(vertex_data_list[0])] = previous_vertex
-        return v_tab, v_number
+        return adjacency_list, vertices_number
 
 
-def write_neighbours_list(first_connected_vertex: ListElement):
-    result = ""
-    connected_vertex = first_connected_vertex
-    while connected_vertex.next:
-        result += str(connected_vertex.v) + ", "
-        connected_vertex = connected_vertex.next
-    result += str(connected_vertex.v)
+def write_neighbours_list(adjacency_list):
+    print("Lista sąsiedztwa:")
+    vertex_result = ""
+    for i in range (0, len(adjacency_list)):
+        for vertex in adjacency_list[i][:-1]:
+            vertex_result += str(vertex) + ", "
+        vertex_result += str(adjacency_list[i][-1])
 
-    return result
+        print(f"Sąsiadami wierzchołka {i} są: {vertex_result}")
+        vertex_result = ""
+
+
+def list_to_matrix(adjacency_list):
+    vertices_number = len(adjacency_list)
+    matrix = [[0 for _ in range(vertices_number)] for _ in range (vertices_number)]
+
+    for i in range(0, len(adjacency_list)):
+        for vertex in adjacency_list[i]:
+            matrix[i][vertex] = 1
+    
+    return matrix
+
+
+def write_matrix(matrix):
+    print("Macierz sąsiedztwa:")
+    vertices_number = len(matrix)
+
+    print("   ", end="")
+    for i in range(vertices_number):
+        print("%3d" % i, end="")
+
+    print()
+    print()
+
+    for i in range(vertices_number):
+        print("%3d" % i, end="")
+        for j in range(vertices_number):
+            print("%3d" % matrix[i][j], end="")
+        print()
+
+
+def main():
+    adjacency_list, vertces_number = read_graph("graph.txt")
+    write_neighbours_list(adjacency_list)
+    matrix = list_to_matrix(adjacency_list)
+    write_matrix(matrix)
+
+
+if __name__ == "__main__":
+    main()
